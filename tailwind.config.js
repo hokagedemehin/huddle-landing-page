e8eb2d1848c19b7953076741f5ac48f012161792
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2022-06-17 14:36:41 +0100

    feat: assignment completed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    fontFamily: {
+      fam1: ['Open Sans', 'sans-serif'],
+      fam2: ['Poppins', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
